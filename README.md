75394e10e736f3d84882d4afe703703704e31ec3
Gabriel S <sirlopug@gmail.com>
2022-03-03 23:58:21 -0800

    deploying a contract with Python & Web3

---
+++ README.md
@@ -0,0 +1,33 @@
+# Notes
+
+To install ganache-cli
+```bash
+npm install -g ganache-cli
+```
+
+Run ganache-cli (keep same keys for each session)
+
+```bash
+ganache-cli --deterministic
+```
+---
+Set your private keys and address, and adjust this section appropriately:
+
+```python
+## For connecting to ganache
+w3 = Web3(Web3.HTTPProvider("http://0.0.0.0:8545"))
+chaind_id = 1337
+my_address = "0x94B806BB0e455576ea46193D9DBbB08d1cc57Da9"
+private_key = os.getenv("PRIVATE_KEY")
+```
+
+To set your private key as an environment variable, create .env file in your project with the PRIVATE_KEY variable
+
+```python
+private_key = os.getenv("PRIVATE_KEY")
+```
+
+Run 
+```bash
+python deploy.py
+```
---
+++ deploy.py
@@ -0,0 +1,111 @@
+from solcx import compile_standard, install_solc
+import json
+import os
+from web3 import Web3
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# print("Installing...")
+install_solc("0.6.0")
+
+with open("./SimpleStorage.sol", "r") as file:
+    simple_storage_file = file.read()
+    # print(simple_storage_file)
+
+# Solidity source code
+compiled_sol = compile_standard(
+    {
+        "language": "Solidity",
+        "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
+        "settings": {
+            "outputSelection": {
+                "*": {
+                    "*": ["abi", "metadata", "evm.bytecode", "evm.bytecode.sourceMap"]
+                }
+            }
+        },
+    },
+    solc_version="0.6.0",
+)
+
+# print(compiled_sol)
+
+with open("compiled_code.json", "w") as file:
+    json.dump(compiled_sol, file)
+
+# get bytecode
+bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"][
+    "bytecode"
+]["object"]
+
+# print(bytecode)
+
+# get abi
+abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
+
+# print(abi)
+
+# for connecting to ganache
+w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
+chain_id = 1337
+my_address = "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1"  ## test address
+private_key = os.getenv("PRIVATE_KEY")  ## test private key
+
+# create the contract in python
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+# print(SimpleStorage)
+
+# Get the latest transaction
+nonce = w3.eth.getTransactionCount(my_address)
+# print(nonce)
+
+# 1. build a transaction
+transaction = SimpleStorage.constructor().buildTransaction(
+    {
+        "chainId": chain_id,
+        "gasPrice": w3.eth.gas_price,
+        "from": my_address,
+        "nonce": nonce,
+    }
+)
+# 2. sign a transaction
+signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+
+# 3. send a transaction
+print("Deploying contract...")
+tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+print("Deployed!")
+# working with the contract
+# contract Address
+# contract ABI
+simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+# Call -> simulate making the call and getting a return value - does not make a state change
+# Transact -> make a state change
+
+# initial value of favorite number
+print(simple_storage.functions.retrieve().call())
+print("Updating Contract...")
+# create transaction
+store_transaction = simple_storage.functions.store(15).buildTransaction(
+    {
+        "chainId": chain_id,
+        "gasPrice": w3.eth.gas_price,
+        "from": my_address,
+        "nonce": nonce + 1,
+    }
+)
+
+# sign transaction
+signed_store_txn = w3.eth.account.sign_transaction(
+    store_transaction, private_key=private_key
+)
+
+# send transaction
+send_store_tx = w3.eth.send_raw_transaction(signed_store_txn.rawTransaction)
+
+# wait for the transaction to finish
+tx_receipt = w3.eth.wait_for_transaction_receipt(send_store_tx)
+print("Updated!")
+print(simple_storage.functions.retrieve().call())
